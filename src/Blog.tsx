d71243e6fa0dac7e5f4d86984ba68317dfc30abc
eco <erinconeal@outlook.com>
2021-07-23 15:34:09 -0400

    Combine images & text to lazy load.

--- src/Blog.tsx
+++ src/Blog.tsx
@@ -4,28 +4,19 @@ import {
   FunctionComponent,
   useReducer,
   useRef,
-  Dispatch,
+  // Dispatch,
 } from "react";
 import { PostsAPIResponse, PicsumPhotosAPIResponse } from "./APIResponsesTypes";
 import Skeleton from "react-loading-skeleton";
 import useInfiniteScroll from "./useInfiniteScroll";
 import useLazyLoading from "./useLazyLoading";
 
-interface CombinedData {
+export interface CombinedData {
   id: string;
   title: string;
   body: string;
   download_url: string;
 }
-
-type imgState = {
-  images: PicsumPhotosAPIResponse[];
-  fetching: boolean;
-};
-
-type imgAction =
-  | { type: "STACK_IMAGES"; images: PicsumPhotosAPIResponse[] }
-  | { type: "FETCHING_IMAGES"; fetching: boolean };
 
 type pageState = {
   page: number;
@@ -39,16 +30,10 @@ const localCache: {
 
 const Blog: FunctionComponent = () => {
   const [data, setData] = useState<CombinedData[]>([]);
-  const imgReducer = (state: imgState, action: imgAction) => {
-    switch (action.type) {
-      case "STACK_IMAGES":
-        return { ...state, images: state.images.concat(action.images) };
-      case "FETCHING_IMAGES":
-        return { ...state, fetching: action.fetching };
-      default:
-        return state;
-    }
-  };
+  const [isFetchingData, setIsFetchingData] = useState(false);
+  const [images, setImages] = useState<PicsumPhotosAPIResponse[]>([]);
+  const [posts, setPosts] = useState<PostsAPIResponse[]>([]);
+  const bottomBoundaryRef = useRef(null);
 
   const pageReducer = (state: pageState, action: pageAction) => {
     switch (action.type) {
@@ -59,67 +44,67 @@ const Blog: FunctionComponent = () => {
     }
   };
 
-  const [pager, pagerDispatch] = useReducer(pageReducer, { page: 0 });
-  const [imgData, imgDispatch] = useReducer(imgReducer, {
-    images: [],
-    fetching: true,
-  });
-
-  const bottomBoundaryRef = useRef(null);
-  useLazyLoading("XXXX", imgData.images);
-  useInfiniteScroll(bottomBoundaryRef, pagerDispatch);
+  const [pager, pagerDispatch] = useReducer(pageReducer, { page: 1 });
 
   useEffect(() => {
-    if (!localCache.blogData.length) {
-      setData(Array(10).fill(undefined));
-      void requestData();
-    } else {
-      setData(localCache.blogData);
-    }
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    // if (!localCache.blogData.length) {
+    //   setData(Array(10).fill(undefined));
+    void requestData(pager.page);
+    console.log("images after concat", images);
+    console.log("posts after concat", posts);
+    console.log("data", data);
+    // } else {
+    //   setData(localCache.blogData);
+    // }
+  }, [pager.page]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  async function requestPosts() {
+  useLazyLoading(".card-top", data);
+  useInfiniteScroll(bottomBoundaryRef, pagerDispatch);
+
+  async function requestPosts(page: number) {
     try {
       // from https://jsonplaceholder.typicode.com/
       const res = await fetch(
-        "https://jsonplaceholder.typicode.com/posts?_limit=10"
+        `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=10`
       );
       const json = (await res.json()) as PostsAPIResponse[];
-      return json || [];
+      setPosts((posts) => posts.concat(json));
+      return Promise.resolve();
     } catch (error) {
       console.log(error);
     }
   }
 
-  async function requestImages(data: pageState, dispatch: Dispatch<imgAction>) {
+  async function requestImages(page: number) {
     try {
-      dispatch({ type: "FETCHING_IMAGES", fetching: true });
       // from https://picsum.photos/
       const res = await fetch(
-        `https://picsum.photos/v2/list?page=${data.page}&limit=10`
+        `https://picsum.photos/v2/list?page=${page}&limit=10`
       );
       const json = (await res.json()) as PicsumPhotosAPIResponse[];
-      dispatch({ type: "STACK_IMAGES", images: json });
-      dispatch({ type: "FETCHING_IMAGES", fetching: false });
-      return json || [];
+      setImages((images) => images.concat(json));
+      return Promise.resolve();
     } catch (error) {
       console.log(error);
-      dispatch({ type: "FETCHING_IMAGES", fetching: false });
     }
   }
 
-  async function requestData() {
-    const [posts, images] = await Promise.all([
-      requestPosts(),
-      requestImages(pager, imgDispatch),
-    ]);
-    const combinedData: CombinedData[] = [];
-    if (posts && posts.length && images && images.length) {
+  async function requestData(page: number) {
+    try {
+      setIsFetchingData(true);
+      await Promise.all([requestPosts(page), requestImages(page)]);
+      const combinedData: CombinedData[] = [];
+      // if (posts && posts.length && images && images.length) {
       posts.forEach((item: PostsAPIResponse, i: number) => {
         combinedData.push(Object.assign({}, item, images[i]));
       });
       setData(combinedData);
       localCache.blogData = combinedData;
+      setIsFetchingData(false);
+      // }
+    } catch (error) {
+      console.log(error);
+      setIsFetchingData(false);
     }
   }
 
@@ -129,9 +114,13 @@ const Blog: FunctionComponent = () => {
       <ul className="grid grid-flow-cols sm:grid-cols-2 blogs">
         {data.map((d, index) => {
           return (
-            <li key={index} className="pr-5">
+            <li key={index} className="pr-5 card-top">
               {d?.download_url ? (
-                <img alt="" src={d.download_url} />
+                <img
+                  alt=""
+                  src={"https://picsum.photos/id/870/300/300?grayscale&blur=2"}
+                  data-src={d.download_url}
+                />
               ) : (
                 <Skeleton height={300} width={300} />
               )}
@@ -141,6 +130,16 @@ const Blog: FunctionComponent = () => {
           );
         })}
       </ul>
+      {isFetchingData && (
+        <div className="text-center bg-secondary m-auto p-3">
+          <p className="m-0 text-white">Getting data</p>
+        </div>
+      )}
+      <div
+        id="page-bottom-boundary"
+        style={{ border: "1px solid red" }}
+        ref={bottomBoundaryRef}
+      ></div>
     </div>
   );
 };
--- src/useLazyLoading.tsx
+++ src/useLazyLoading.tsx
@@ -1,23 +1,26 @@
 import { useEffect, useCallback, useRef, MutableRefObject } from "react";
-import { PicsumPhotosAPIResponse } from "./APIResponsesTypes";
+import { CombinedData } from "./Blog";
 
 // lazy load images with intersection observer
 const useLazyLoading = (
-  imgSelector: string,
-  items: PicsumPhotosAPIResponse[]
+  elementSelector: string,
+  items: CombinedData[]
 ): void => {
-  const imgObserver = useCallback((node) => {
+  const observer = useCallback((node) => {
     const intObs = new IntersectionObserver((entries) => {
       entries.forEach((en) => {
         if (en.intersectionRatio > 0) {
-          const currentImg = en.target as HTMLImageElement;
-          const newImgSrc = currentImg.dataset.src;
+          const currentListItem = en.target as HTMLLIElement;
+          console.log("current list item", currentListItem.children);
+          if (currentListItem.children[0].tagName === "IMG") {
+            const newImgSrc = currentListItem.children[0].dataset.src;
 
-          // only swap out the image source if the new url exists
-          if (!newImgSrc) {
-            console.error("Image source is invalid");
-          } else {
-            currentImg.src = newImgSrc;
+            // only swap out the image source if the new url exists
+            if (!newImgSrc) {
+              console.error("Image source is invalid");
+            } else {
+              currentListItem.children[0].src = newImgSrc;
+            }
           }
           intObs.unobserve(node);
         }
@@ -26,15 +29,15 @@ const useLazyLoading = (
     intObs.observe(node);
   }, []);
 
-  const imagesRef: MutableRefObject<HTMLImageElement[] | null> = useRef(null);
+  const imagesRef: MutableRefObject<HTMLLIElement[] | null> = useRef(null);
 
   useEffect(() => {
-    imagesRef.current = Array.from(document.querySelectorAll(imgSelector));
+    imagesRef.current = Array.from(document.querySelectorAll(elementSelector));
 
     if (imagesRef.current) {
-      imagesRef.current.forEach((img: HTMLImageElement) => imgObserver(img));
+      imagesRef.current.forEach((img: HTMLLIElement) => observer(img));
     }
-  }, [imgObserver, imagesRef, imgSelector, items]);
+  }, [observer, imagesRef, elementSelector, items]);
 };
 
 export default useLazyLoading;
