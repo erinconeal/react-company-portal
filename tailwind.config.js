78150b879fb24a20e1bd37667db05039b0d1c245
eco <erinconeal@outlook.com>
2021-08-16 11:49:22 -0400

    Fix CORS pic issue.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: [],
+  purge: ["./src/**/*.html", "./src/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
