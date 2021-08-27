3942a208c966a7465a26abe078983f080faa3380
eco <erinconeal@outlook.com>
2021-08-27 10:27:16 -0400

    Convert Fontawesome file to ts.

--- src/fontawesome.ts
+++ src/fontawesome.ts
@@ -1,6 +1,6 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faAtom } from "@fortawesome/free-solid-svg-icons";
 
-export default function registerIcons() {
+export default function registerIcons(): void {
   library.add(faAtom);
 }
